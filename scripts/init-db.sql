dd0c00b02010f7ec74d9fd0b0f63b2d51e3103aa
eml-12-code <erickleung12@gmail.com>
2026-05-06 14:14:01 +0800

    Update init-db.sql

--- scripts/init-db.sql
+++ scripts/init-db.sql
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS accounts (
   username VARCHAR(36) NOT NULL,
   password VARCHAR(36) NOT NULL,
   email VARCHAR(36) NOT NULL,
-  date DATE NOT NULL,
+  start_date DATE NOT NULL,
   active ENUM('active', 'cancel') DEFAULT 'active',
   member_level ENUM('1', '2', '3', '4', '5') DEFAULT '1',
   oauth_provider VARCHAR(36) NOT NULL
