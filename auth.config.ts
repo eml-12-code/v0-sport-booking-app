c683d4658df20cc6a784d27faeb3ac16cc700f24
eml-12-code <erickleung12@gmail.com>
2026-05-06 15:46:00 +0800

    Create auth.config.ts

---
+++ auth.config.ts
@@ -0,0 +1,19 @@
+// auth.config.ts
+
+import type { NextAuthConfig } from "next-auth";
+import Google from "next-auth/providers/google";
+
+export default {
+  providers: [Google], // Add other OAuth providers here
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const loggedIn = !!auth?.user;
+      const onLogin = nextUrl.pathname.startsWith('/login');
+      if (onLogin) {
+        if (loggedIn) return Response.redirect(new URL('/', nextUrl));
+        return true;
+      }
+      return loggedIn;
+    },
+  },
+} satisfies NextAuthConfig;
